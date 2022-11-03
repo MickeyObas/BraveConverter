c4195a115a46ef07ffb85e4099d25070e1a33ef4
Michael <mikhzobby@gmail.com>
2022-11-03 08:06:38 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Welcome to the BraveConvert tool :)
+
+> What is this?
+
+I'm glad you asked. This program receives your csv file which is formatted to store NFT entries and their correct fields- *I hope*- and generates a sha-256 hashed JSON file for every single one of them.
+
+> How do I use it?
+
+1. Fork the repository over or simply clone it into your PC.
+2. Place your csv file inside the forked folder.
+3. Open the folder on your command terminal or simply change into its directory.
+4. Install the required dependencies by running `pip install -r requirements.py` on your command line.
+5. Run `braveconvert.py <filename>` on the command line where- `<filename>` is actually your csv file name in quotes and not angle brackets.
+
+I know right, it's THAT easy. Your files have been generated, hashed and properly recomposed into a new csv file.
+
+
+
+
---
+++ braveconvert.py
@@ -0,0 +1,122 @@
+"""
+- Take CSV as input
+- Generate a CHIP-007 compatible JSON file for each CSV input row
+- Generate the sha256 encryption of each file
+- Append the sha256 hash to each line in the original csv (filename.output.csv)
+"""
+
+import csv 
+import json
+import hashlib
+import pandas as pd
+
+import os
+import argparse
+
+# Error messages 
+INVALID_FILETYPE_MSG = "Error: Whoops. Invalid file format. %s must be a .csv file."
+INVALID_PATH_MSG = "Error: Whoops. Invalid file/path name. Path %s does not exist."
+
+def validate_file(file_name):
+    '''
+    Validate file name and path.
+    '''
+    if not valid_filetype(file_name):
+        print(INVALID_PATH_MSG%(file_name))
+        print("If it does, it sure doesn't look like a CSV file. Please try again")
+        quit()
+    return
+
+def valid_filetype(file_name):
+    # Validate file type
+    return file_name.endswith('.csv')
+
+def valid_path(path):
+    # Validate file type
+    return os.path.exists(path)
+
+
+def main():
+    # Create parser object
+    parser = argparse.ArgumentParser(description='Process your CSV file here and get the required output')
+    # Defining argument(s)
+    parser.add_argument("file", type=str, nargs=1, help="Reads your NFT csv file, generates the json files for each NFT, hashes them, then returns an updated CSV.",
+    metavar="file_name", default=None)
+
+    args = parser.parse_args()
+
+    if args.file != None:
+        validate_file(args.file[0])
+        file_name = args.file[0]
+
+    # I initiate an empty list which i'll temporarily use to hold hash values
+    hash_list = []
+
+    with open(file_name, 'r') as f:
+        reader = csv.reader(f)
+
+        for row in reader:
+            series_number = row[0]
+            filename = row[1]
+            name= row[2]
+            description= row[3]
+            gender = row[4]
+            attributes = row[5]
+            uuid = row[6]
+
+            # I create a response model using the format given by the HNG9 mentors, then named it 'nft'
+            nft = {
+                "format": "CHIP-0007",
+                "name": name,
+                "description": description,
+                "minting_tool": "Team x",
+                "sensitive_content": False,
+                "series_number": series_number,
+                "series_total": 420,
+                "attributes": [
+                    {
+                        "trait_type": "gender",
+                        "value": gender
+                    }
+                ],
+                "collection": {
+                    "name": "Zuri NFT tickets for free lunch",
+                    "id": uuid,
+                    "attributes": [
+                        {
+                            "type": "description",
+                            "value": "Rewards for accomplishments during HNGi9"
+                        }
+                    ]
+                },
+            }
+
+            # Each nft entry is dumped on its own '.json' file
+            with open("{}.json".format(filename), 'w') as outfile:
+                json.dump(nft, outfile, indent=4, separators=(", ", ": "))
+                outfile.close()
+
+            # This line of code prevents the script from hashing what it's not supposed to hash
+            if filename == "Filename":
+                pass
+
+            else:
+                with open("{}.json".format(filename), "rb") as f:
+                    bytes = f.read()
+                    readable_hash = hashlib.sha256(bytes).hexdigest()
+                    hash_list.append(readable_hash)
+                    f.close()
+        
+        # The newly generated sha256 hash values for each nft file is appended their respective rows in the original csv file, then exported to a new one
+        csv_input = pd.read_csv(file_name)
+        csv_input['Hash'] = hash_list
+    
+        csv_input.to_csv('filename.output.csv', index=False)
+
+
+if __name__ == "__main__":
+    # Calling the main function
+    main()
+
+
+        
